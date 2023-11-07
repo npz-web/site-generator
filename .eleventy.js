1afbc62ffbdd8e5cf2357634b03a839a3e15c4bd
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-11-07 04:38:10 +0000

    adds devcontainer setup, sample includes for footer, head, header

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,15 @@
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("src");
+    eleventyConfig.addWatchTarget("./src/");
+  
     
     // Return your Object options:
-
-
     return {
       dir: {
         input: "src",
-        output: "docs"
+        output: "docs",
+        includes: "_includes",
       }
     }
   };
---
+++ src/_includes/footer.liquid
@@ -0,0 +1,7 @@
+<footer>
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+    </ul>
+</footer>
