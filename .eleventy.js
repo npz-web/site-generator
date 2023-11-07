f7befb713046e3c8bb15424e47d691f4846ac349
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-11-07 03:14:31 +0000

    adds passthrough file copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy("src");
+    
     // Return your Object options:
+
+
     return {
       dir: {
         input: "src",
